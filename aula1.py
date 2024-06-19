e305ed45efb55da62c045410500447f6a987b942
BrunoBorges79 <bruuno.borges2024@gmail.com>
2024-06-19 13:17:11 -0300

    Create aula1.py
    meu primeiro teste

---
+++ aula1.py
@@ -0,0 +1,10 @@
+velocidade = 50
+contador = 0
+
+aumentar = velocidade + 10
+diminuir = velocidade - 10
+
+motor = float(input(" A = aumentar, D diminuir:"))
+op = int(input("Digite A para aumentar\n"
+               "Digite D para diminuir\n"
+               "digite o que fazer:"))
