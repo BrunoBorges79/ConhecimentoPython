37658118b29586caf98068c010e7269361edd2c3
BrunoBorges79 <bruuno.borges2024@gmail.com>
2024-06-26 16:55:01 -0300

    Create prova.py
    prova pratica

---
+++ prova.py
@@ -0,0 +1,99 @@
+# valor = int(input("Digite seu valor:"))
+#
+# if valor % 2 == 0:
+#     print("seu valor é par")
+#
+# else:
+#     print("seu valor é impar")
+
+# valor = int(input("Digite seu valor:"))
+#
+# if valor >= 1:
+#     print("positivo")
+#
+# else:
+#     print("negativo")
+
+# valor1 = int(input("Digite valor1:"))
+# valor2 = int(input("Digite valor2:"))
+#
+# if valor1 - valor2 == 0:
+#     print("são iguais")
+#
+# else:
+#     print("não são iguais")
+
+# valor = int(input("Digite um valor:"))
+#
+# if valor >= 25  <= 37:
+#     print("esta dentro")
+#
+# else:
+#     print("esta fora")
+
+# valor1 = int(input("Digite valor1:"))
+# valor2 = int(input("Digite valor2:"))
+#
+# if valor1 % valor2 == 0:
+#
+#     print("O número é múltiplo")
+#
+# else:
+#
+#     print("O número não é múltiplo")
+
+# valor1 = int(input("Digite valor1:"))
+# valor2 = int(input("Digite valor2:"))
+# valor3 = int(input("Digite valor3:"))
+# valor4 = int(input("Digite valor4:"))
+#
+# resultado = valor1 + valor2 + valor3 +valor4
+#
+# print(resultado)
+
+# valor1 = int(input("Digite valor1:"))
+# valor2 = int(input("Digite valor2:"))
+#
+# resultado = valor1 % valor2
+#
+# print(resultado)
+
+# valor1 = int(input("Digite valor1:"))
+# valor2 = int(input("Digite valor2:"))
+#
+# resultado = valor1 ** valor2
+#
+# print(resultado)
+
+# altura1 = float(input("Digite altura1:"))
+# altura2 = float(input("Digite altura2:"))
+# altura3 = float(input("Digite altura3:"))
+#
+# media = (altura1 +altura2 +altura3)/2
+#
+# print("A media é {} :".format(media))
+
+# valor1 = float(input("Digite valor1:"))
+#
+# temperatura= valor1 - - 32 * 5/9
+#
+# print(temperatura)
+
+
+# liquido = int(input("Digite valor1:"))
+# bruto = int(input("Digite valor2:"))
+# desconto = int(input("Digite valor3:"))
+#
+# resultado1 = liquido + bruto
+#
+# resultado2 = resultado1 *desconto/100
+#
+# print(resultado2)
+
+velocidade = float(input("Digite velocidade:"))
+distancia = float(input("Digite distancia:"))
+tempo = float(input("Digite tempo:"))
+
+media = (velocidade + distancia + tempo )/2
+
+print("A media é {} :".format(media))
