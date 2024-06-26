df650f0f1cc1425b7b424ef7c77719fd2be009fc
BrunoBorges79 <bruuno.borges2024@gmail.com>
2024-06-26 16:50:49 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -1,10 +1,52 @@
-velocidade = 50
-contador = 0
+import tkinter as interface
+import menu
+from tkinter import messagebox
 
-aumentar = velocidade + 10
-diminuir = velocidade - 10
+janela = None
+txtusuario = None
+txtsenha = None
 
-motor = float(input(" A = aumentar, D diminuir:"))
-op = int(input("Digite A para aumentar\n"
-               "Digite D para diminuir\n"
-               "digite o que fazer:"))
+def Abrinovajanela():
+    usuario = txtusuario.get()
+    senha = txtsenha.get()
+
+    if usuario == "admin" and senha == "4321":
+        messagebox.showinfo("Sucesso", "Acesso bem-sucedido")
+        janela.destroy()
+        menu.iniciarJanela()
+    else:
+        messagebox.showinfo("Erro", "Acesso negado")
+
+def iniciarJanela():
+    global txtusuario
+    global txtsenha
+    global janela
+    janela = interface.Tk()
+    janela.geometry("400x400")
+    janela.title("Exemplo de Tela com Botões")
+
+    frame1 = interface.Frame(janela)
+    frame1.pack()
+
+    frameBotao = interface.Frame(janela)
+    frameBotao.pack()
+
+    ldlusuario = interface.Label(frame1, text="Usuario")
+    ldlusuario.grid(row=0, column=0)
+
+    txtusuario = interface.Entry(frame1)
+    txtusuario.grid(row=0, column=1)
+
+    ldlsenha = interface.Label(frame1, text="Senha")
+    ldlsenha.grid(row=2, column=0)
+
+    txtsenha = interface.Entry(frame1)
+    txtsenha.grid(row=2, column=1)
+
+    btnCadastro = interface.Button(frameBotao, text="Cadastrar", command= Abrinovajanela)
+    btnCadastro.pack(pady=10)
+
+    janela.mainloop()
+
+if __name__ == "__main__":
+    iniciarJanela()
